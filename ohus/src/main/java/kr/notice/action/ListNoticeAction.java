c84d6c639109a14c6e433f9f2f9919939cfd1956
user <user@DESKTOP-2KP3VTQ>
2023-06-20 16:00:42 +0900

    feat: 공지사항 목록 업로드

---
+++ ohus/src/main/java/kr/notice/action/ListNoticeAction.java
@@ -0,0 +1,38 @@
+package kr.notice.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+import kr.notice.dao.NoticeDAO;
+import kr.notice.vo.NoticeVO;
+import kr.util.PageUtil;
+
+public class ListNoticeAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String pageNum = request.getParameter("pageNum");
+		if(pageNum == null) pageNum = "1";
+		
+		String keyfield = request.getParameter("keyfield");
+		String keyword = request.getParameter("keyword");
+		
+		NoticeDAO dao = NoticeDAO.getInstance();
+		int count = dao.getNoticeCount(keyfield, keyword);
+		
+		PageUtil page = new PageUtil(keyfield, keyword, Integer.parseInt(pageNum), count, 20, 10, "listNotice.do");
+		List<NoticeVO> list = null;
+		if(count > 0) {
+			list = dao.getListNotice(page.getStartRow(), page.getEndRow(), keyfield, keyword);
+		}
+		request.setAttribute("count", count);
+		request.setAttribute("list", list);
+		request.setAttribute("page", page.getPage());
+		
+		return "/WEB-INF/views/notice/listNotice.jsp";
+	}
+
+}
