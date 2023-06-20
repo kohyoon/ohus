ec56a4defce977f3a0376ef0d30ff66cba7620db
user <user@DESKTOP-2KP3VTQ>
2023-06-20 12:39:40 +0900

    feat: InquiryAnswerVO.java 업로드

---
+++ ohus/src/main/java/kr/inquiry/vo/InquiryAnswerVO.java
@@ -0,0 +1,49 @@
+package kr.inquiry.vo;
+
+import java.sql.Date;
+
+public class InquiryAnswerVO {
+	private int ans_num;
+	private String ans_content;
+	private Date ans_date;
+	private int inq_num;
+	private int mem_num; //답변한 관리자 회원번호
+	private String id; //답변한 관리자 아이디
+	
+	public int getAns_num() {
+		return ans_num;
+	}
+	public void setAns_num(int ans_num) {
+		this.ans_num = ans_num;
+	}
+	public String getAns_content() {
+		return ans_content;
+	}
+	public void setAns_content(String ans_content) {
+		this.ans_content = ans_content;
+	}
+	public Date getAns_date() {
+		return ans_date;
+	}
+	public void setAns_date(Date ans_date) {
+		this.ans_date = ans_date;
+	}
+	public int getInq_num() {
+		return inq_num;
+	}
+	public void setInq_num(int inq_num) {
+		this.inq_num = inq_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
