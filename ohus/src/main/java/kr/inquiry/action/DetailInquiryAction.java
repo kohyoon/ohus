3d5e50c212de22522736fa7da4ffb114bb4cbcab
user <user@DESKTOP-2KP3VTQ>
2023-06-21 11:48:24 +0900

    docs: DetailInquiryAction 수정

--- ohus/src/main/java/kr/inquiry/action/DetailInquiryAction.java
+++ ohus/src/main/java/kr/inquiry/action/DetailInquiryAction.java
@@ -2,6 +2,7 @@ package kr.inquiry.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
 import kr.inquiry.dao.InquiryDAO;
@@ -12,6 +13,16 @@ public class DetailInquiryAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//로그인 여부 체크
+		HttpSession session = request.getSession();
+		Integer user_num = (Integer)session.getAttribute("user_num");
+		if(user_num == null) { //로그인 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
+		
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
+		request.setAttribute("user_auth", user_auth);
+		
 		//문의글 번호
 		int inq_num = Integer.parseInt(request.getParameter("inq_num"));
 		InquiryDAO dao = InquiryDAO.getInstance();
