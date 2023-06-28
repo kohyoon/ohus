a6dc8ef6862a413f2a40ca59827f69a662460ce1
user <user@DESKTOP-2KP3VTQ>
2023-06-28 15:15:31 +0900

    fix: 에러 수정 재업로드

---
+++ ohus/src/main/java/kr/item/action/UserDetailAction.java
@@ -0,0 +1,46 @@
+package kr.item.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+import kr.item.dao.ItemDAO;
+import kr.item.vo.ItemReviewVO;
+import kr.item.vo.ItemVO;
+import kr.util.PageUtil;
+import kr.util.StringUtil;
+
+public class UserDetailAction implements Action{
+
+   @Override
+   public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+      //상품 번호 반환
+      int item_num = Integer.parseInt(request.getParameter("item_num"));
+      
+      ItemDAO dao = ItemDAO.getInstance();
+      
+      //상품 정보 가져오기
+      ItemVO item = dao.getItem(item_num);
+      //내용 줄바꿈 처리
+      item.setItem_content(StringUtil.useBrHtml(item.getItem_content()));
+      
+      //조회수 증가
+      dao.updateViewsCount(item_num);
+      
+      //리뷰 개수
+      int reviewCount = dao.getReviewCount(item_num);
+      
+     
+      
+      double avgscore = dao.avgReview(item_num);
+      
+      request.setAttribute("avgscore", avgscore);
+      request.setAttribute("reviewCount", reviewCount);
+      request.setAttribute("item", item);
+      
+      return "/WEB-INF/views/item/user_detail.jsp";
+   }
+
+}
