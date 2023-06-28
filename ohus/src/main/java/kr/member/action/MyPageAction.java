e458df2a109b5c747445adf0fcc54cb8f1ebf340
d9751 <d9751@LAPTOP-5639DIPC>
2023-06-28 12:20:47 +0900

    feat : 회원 마이페이지

--- ohus/src/main/java/kr/member/action/MyPageAction.java
+++ ohus/src/main/java/kr/member/action/MyPageAction.java
@@ -1,3 +1,5 @@
+
+
 package kr.member.action;
 
 import java.util.List;
@@ -6,67 +8,77 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import kr.community.dao.CommunityDAO;
+import kr.community.vo.CommunityVO;
 import kr.controller.Action;
-import kr.item.dao.ItemDAO;
-import kr.item.vo.ItemVO;
 import kr.member.dao.MemberDAO;
 import kr.member.vo.MemberVO;
-import kr.order.dao.OrderDAO;
-import kr.order.vo.OrderVO;
-import kr.util.PageUtil;
-
-public class MyPageAction implements Action{
+//[프로필]
+//프로필 사진 수정 -- member
+//커뮤니티에 올린 사진+스크랩한 글 --commnunity
+//상추글 --market
+//문의내용 --inquiry
+public class MyPageAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		//로그인 여부 체크
+		
+		//로그인 여부 + 회원 체크
 		HttpSession session = request.getSession();
-		Integer user_num = 
-			(Integer)session.getAttribute("user_num");
+		Integer user_num = (Integer)session.getAttribute("user_num");
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
+		
 		if(user_num==null) {//로그인이 되지 않은 경우
 			return "redirect:/member/loginForm.do";			
 		}
-		
-		
-		//로그인이 된 경우
-		MemberDAO dao = MemberDAO.getInstance();
-		//회원정보
-		MemberVO member = dao.getMember(user_num);
-		
-		/*
-		 * BoardDAO boardDao = BoardDAO.getInstance(); //게시판 글을 마이 페이지에서 보여주기
-		 * List<BoardVO> boardList = boardDao.getListBoardFav(1,5, user_num);
-		 * 
-		 * request.setAttribute("member", member); 
-		 * request.setAttribute("boardList", boardList);
-		 */
-		
-		//상품 스크랩
-		ItemDAO itemDao = ItemDAO.getInstance();
-		List<ItemVO> itemList = itemDao.getListItemFav(1, 5, user_num);
-		
-		// 주문 정보
-		OrderDAO orderDao = OrderDAO.getInstance();
-		List<OrderVO> orderList = orderDao.getListOrderByMem_num(1, 5, null, null, user_num);
-						
+		if(user_auth !=2) {
+			return "/WEB-INF/views/common/notice.jsp";
+		}
 		
 		//분리한 마이페이지 처리
 		String page = request.getParameter("page");
+		
+		//---------------------------------------------------------
+		//회원 프로필 사진 수정 
+		MemberDAO dao = MemberDAO.getInstance();
+		MemberVO member = dao.getMember(user_num);
+		
+		
+		//---------------------------------------------------------
+		//내가 커뮤니티에 올린 사진 + 좋아요 누른 글
+		
+		 CommunityDAO commDao = CommunityDAO.getInstance();
+		 
+		 //좋아요 누른 글
+		 List<CommunityVO> commList = commDao.getListBoardFav(1,5, user_num);
+		
+		//---------------------------------------------------------
+		//내가 작성한 상추글
+		
+		//---------------------------------------------------------
+		//내가 작성한 문의내용
+		
+		
+		
+		
+		  
+		
+		
+		
+						
+		
+		
 
 				
-				
-			    // 페이지 정보를 request 객체에 추가
+		//---------------------------------------------------------		
+		// 페이지 정보 request 객체에 추가
 		request.setAttribute("page", page);
-		request.setAttribute("member", member);
-		request.setAttribute("itemList", itemList);
-		request.setAttribute("orderList", orderList);
+		 request.setAttribute("member", member); 
+		 request.setAttribute("commList", commList);
 		
 		
 		
 		
-
-	
 		//JSP 경로 반환
 		return "/WEB-INF/views/member/myPage.jsp";
 	}
---
+++ ohus/src/main/java/kr/member/action/MyPageAction2.java
@@ -0,0 +1,52 @@
+package kr.member.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.controller.Action;
+import kr.event.dao.EventDAO;
+import kr.event.vo.EventReplyVO;
+//[나의 참여]
+//내가 작성한 리뷰 - order
+//응모내역 - event
+public class MyPageAction2 implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		//로그인 여부 + 회원 체크
+		HttpSession session = request.getSession();
+		Integer user_num = (Integer)session.getAttribute("user_num");
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
+		
+		if(user_num==null) {//로그인이 되지 않은 경우
+			return "redirect:/member/loginForm.do";			
+		}
+		if(user_auth !=2) {
+			return "/WEB-INF/views/common/notice.jsp";
+		}
+		 request.setCharacterEncoding("utf-8");
+		
+		//---------------------------------------------------------	
+		//내가 작성한 상품 리뷰
+		
+		
+		
+		//---------------------------------------------------------	
+		//내가 참여한 이벤트 응모 내역(댓글)
+		EventDAO eventDao = EventDAO.getInstance();
+        List<EventReplyVO> mylist = new ArrayList<EventReplyVO>();
+        mylist = eventDao.getEventRepliesByMember(user_num);
+
+		//---------------------------------------------------------		
+		// 페이지 정보 request 객체에 추가
+        request.setAttribute("mylist", mylist);
+		
+		return "/WEB-INF/views/member/myPage2.jsp";
+	}
+
+}
