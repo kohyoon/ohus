6f7d426efaab7c926e82c517dc3c448adbf47b9d
user <user@DESKTOP-2KP3VTQ>
2023-06-29 15:18:43 +0900

    feat: 상품문의 삭제 모델클래스 업로드

---
+++ ohus/src/main/java/kr/qna/action/DeleteAnswerAction.java
@@ -0,0 +1,55 @@
+package kr.qna.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import kr.controller.Action;
+import kr.qna.dao.ItemQnaDAO;
+import kr.qna.vo.ItemAnswerVO;
+
+public class DeleteAnswerAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//전송된 데이터 인코딩 처리
+		request.setCharacterEncoding("utf-8");
+		//전송된 데이터 반혼
+		int a_num = Integer.parseInt(request.getParameter("a_num"));
+		
+		Map<String, String> mapAjax = new HashMap<String, String>();
+		
+		ItemQnaDAO dao = ItemQnaDAO.getInstance();
+		ItemAnswerVO db_answer = dao.getAnswer(a_num);
+		
+		HttpSession session = request.getSession();
+		Integer user_num = (Integer)session.getAttribute("user_num");
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
+		if(user_num == null) {
+			mapAjax.put("result", "logout");
+		} else if(user_num != null && user_auth == 9) {
+			dao.deleteAnswer(a_num);
+			
+			
+			
+			mapAjax.put("result", "success");
+		}else {
+			mapAjax.put("result", "wrongAccess");
+		}
+		
+		//JSON 데이터 생성
+		ObjectMapper mapper = new ObjectMapper();
+		String ajaxData = mapper.writeValueAsString(mapAjax);
+		
+		request.setAttribute("ajaxData", ajaxData);
+		
+		//JSP 경로 반환
+		return "/WEB-INF/views/common/ajax_view.jsp";
+	}
+
+}
